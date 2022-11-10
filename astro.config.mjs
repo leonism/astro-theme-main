a9b6a0f348f408607125fe5709c8c10a17f09d1f
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-10 09:47:59 +0700

    rolling back

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,5 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
-  site: 'https://leonism.github.io/astro-theme-main/',
-  // base: '/my-repo',
-  base: '/astro-theme-main',
+  integrations: [tailwind()]
 });
