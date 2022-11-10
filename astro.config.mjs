9fd234933055ca5e5cd6484fc790bceac9c24c3f
gerry leo nugroho <gerryleonugroho@gmail.com>
2022-11-10 09:22:50 +0700

    adding deploy.yml & astro.yml

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()]
+  integrations: [tailwind()],
+  site: 'https://leonism.github.io/astro-theme-main/',
+  // base: '/my-repo',
+  base: '/dist',
 });
